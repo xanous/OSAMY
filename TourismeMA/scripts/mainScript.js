d24499d81ecab78e067515c20a8ce44d81ccf21a
xanous <aoun.anass@gmail.com>
2016-04-08 23:45:44 +0200

    first commit

---
+++ TourismeMA/scripts/mainScript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(e) {   
+    
+$('#letsGo').click(function(e){
+
+	//alert("The paragraph was clicked.");
+	window.location.href ="views/city.html";
+
+
+});
+
+$('#akchour').click(function(e){
+
+	window.location.href ="views/city.html";
+
+});
+
+ });
+
